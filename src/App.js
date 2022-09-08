9a414741d44973198ef150a9dacaff8cdfe8cf45
mramzansabir494 <mramzansabir494@gmail.com>
2022-09-08 13:22:43 +0500

    useReducer first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+    case "DECREMENT":
+      return { ...state, count: state.count - 1 };
+    // case "RESET":
+    //   console.log("Reset .........");
+    //   return { ...state, reset: state.reset };
+    case "NEW_USER_INPUT":
+      return { ...state, userInput: action.payload };
+    case "TOGGLE_COLOR":
+      return { ...state, color: !state.color };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    // reset: 0,
+    userInput: "",
+    color: false,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App" style={{ color: state.color ? "red" : "green" }}>
+      <div className="container">
+        <input
+          placeholder="Type Inside ..."
+          type="text"
+          value={state.userInput}
+          onChange={(e) =>
+            dispatch({ type: "NEW_USER_INPUT", payload: e.target.value })
+          }
+        />
+        <br />
+        <br />
+        <p>Count: {state.count}</p>
+        {/* <p>Reset: {state.reset}</p> */}
+        <section>
+          <button onClick={() => dispatch({ type: "DECREMENT" })}> - </button>
+          <button onClick={() => dispatch({ type: "INCREMENT" })}> + </button>
+          {/* <button onClick={() => dispatch({ type: "RESET" })}>Reset</button> */}
+          <button onClick={() => dispatch({ type: "TOGGLE_COLOR" })}>
+            Color
+          </button>
+        </section>
+        <br />
+        <br />
+        <p>{state.userInput}</p>
+      </div>
+    </main>
   );
 }
 
